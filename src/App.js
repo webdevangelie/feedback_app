3b22c5f4263c7dbcd9081f6ba21e0629c34bca0a
WebDev Journey <angeliesioson@gmail.com>
2022-03-14 17:13:30 +0800

    feedback form completion with animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { v4 as uuidv4 } from "uuid"
 import React, { useState } from "react"
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -13,10 +15,17 @@ function App() {
     }
   }
 
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    console.log(newFeedback)
+    setFeedback([newFeedback, ...feedback])
+  }
+
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
